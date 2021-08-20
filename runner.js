809fc73dbca686a2d192320130b02a578ec7eb4a
gregoriB <brandon.gregori@gmail.com>
2021-08-19 22:20:11 -0400

    Fix runner

--- runner.js
+++ runner.js
@@ -1,18 +1,16 @@
 const TestLib = require('./TestLib');
 const { applyFlags, setCustomLogs, getTestResults } = require('./utils');
 
-function init() {
+function logResults(results) {
+    const testlib = new TestLib();
+    testlib.logTestResults(results);
+}
+
+(function() {
     /*** Function call order matters here ***/
     setCustomLogs();
     const args = process.argv.slice(2);
     applyFlags(args);
     const results = getTestResults(args);
     logResults(results);
-}
-
-function logResults(results) {
-    const testlib = new TestLib();
-    testlib.logTestResults(results);
-}
-
-init();
+})();
